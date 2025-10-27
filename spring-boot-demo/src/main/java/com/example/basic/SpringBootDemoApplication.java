fa03f0dd5b563a24be047e3e5afaf24734751dda
seunghyun <jsh3990@gmail.com>
2025-10-27 16:06:14 +0900

    spring

---
+++ spring-boot-demo/src/main/java/com/example/basic/SpringBootDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+
+        //스프링 켜먼서 하고 싶은 작업이 있으면, 추가...
+	}
+
+}
---
+++ spring-boot-demo/src/main/java/com/example/basic/chap01/command/ReqVO.java
@@ -0,0 +1,67 @@
+package com.example.basic.chap01.command;
+
+//VO(Value Object) = 값을 저장하는 용도의 클래스
+
+import java.util.ArrayList;
+
+public class ReqVO {
+    //1. 멤버변수는 은닉, getter, setter를 생성함
+    private String id;
+    private String pw;
+    private String name;
+    private ArrayList<String> inter;
+
+
+    //alt+ insert -> 컨스트럭터
+    //2. 생성자는 기본생성자, 멤버변수 개수만한 생성자
+    public ReqVO() {}
+    public ReqVO(String id, String pw, String name, ArrayList<String> inter) {
+        this.id = id;
+        this.pw = pw;
+        this.name = name;
+        this.inter = inter;
+    }
+
+    @Override
+    public String  toString() {
+        return "ReqVO{" +
+                "id='" + id + '\'' +
+                ", pw='" + pw + '\'' +
+                ", name='" + name + '\'' +
+                ", inter=" + inter +
+                '}';
+    }
+
+    //3. getter, setter
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getInter() {
+        return inter;
+    }
+
+    public void setInter(ArrayList<String> inter) {
+        this.inter = inter;
+    }
+}
